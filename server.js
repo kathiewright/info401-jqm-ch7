bbea19408fea93a4e240c016b11a46b24783b2fa
Glitch (info401-jqm-ch7) <none>
2018-02-25 15:58:05 +0000

    🐋💽 Updated with Glitch

--- server.js
+++ server.js
@@ -16,37 +16,49 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/test2.html", function (request, response) {
+  response.sendFile(__dirname + '/public/test2.html');
+});
+
 app.get("/test3.html", function (request, response) {
   response.sendFile(__dirname + '/public/test3.html');
 });
 
-app.get("/test5.html", function (request, response) {
-  response.sendFile(__dirname + '/public/test5.html');
+app.get("/test5-custom.html", function (request, response) {
+  response.sendFile(__dirname + '/public/test5-custom.html');
 });
 
 app.get("/test6.html", function (request, response) {
   response.sendFile(__dirname + '/public/test6.html');
 });
 
+app.get("/test7.html", function (request, response) {
+  response.sendFile(__dirname + '/public/test7.html');
+});
+
 app.get("/test8.html", function (request, response) {
   response.sendFile(__dirname + '/public/test8.html');
 });
 
-app.get("/test_resp_adv_table.html", function (request, response) {
-  response.sendFile(__dirname + '/public/test_resp_adv_table.html');
+app.get("/test9.html", function (request, response) {
+  response.sendFile(__dirname + '/public/test10.html');
 });
 
-app.get("/test_panel_display.html", function (request, response) {
-  response.sendFile(__dirname + '/public/test_panel_display.html');
+app.get("/test10.html", function (request, response) {
+  response.sendFile(__dirname + '/public/test10.html');
 });
 
-app.get("/test_filter.html", function (request, response) {
-  response.sendFile(__dirname + '/public/test_filter.html');
+app.get("/test13.html", function (request, response) {
+  response.sendFile(__dirname + '/public/test13.html');
 });
 
-
+//IMPORTANT!  Note that I am using the POST method instead of the GET method for this one....
+app.post("/echo.html", function (request, response) {
+  response.sendFile(__dirname + '/public/echo.html');
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
